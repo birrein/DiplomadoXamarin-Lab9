542618de43a9f8bf1b2bec016e55abaa2d217edb
Manuel Marín <birrein@LIVE.COM>
2017-07-07 22:18:53 -0400

    Add project files.

---
+++ Lab9/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab9
+{
+    [Activity(Label = "Lab9", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            string EMail = "";
+            string Password = "";
+            string Device = Android.Provider.Settings.Secure.GetString(
+                ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+            var UserNameResTextView = FindViewById<TextView>(Resource.Id.UserNameResTextView);
+            var StatusResTextView = FindViewById<TextView>(Resource.Id.StatusResTextView);
+            var TokenResTextView = FindViewById<TextView>(Resource.Id.TokenResTextView);
+
+            Validate();
+
+            async void Validate()
+            {
+                var ServiceClient = new SALLab09.ServiceClient();
+                var SvcResult = await ServiceClient.ValidateAsync(EMail, Password, Device);
+
+                UserNameResTextView.Text = SvcResult.Fullname;
+                StatusResTextView.Text = SvcResult.Status.ToString();
+                TokenResTextView.Text = SvcResult.Token;
+            }
+        }
+    }
+}
+
